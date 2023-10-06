e74e18aee73bfbb4097c5c8fa337d31eb5d1cb15
sheep-realms <sheep_realms@qq.com>
2023-10-06 14:08:01 +0800

    更新编辑器表单构造器，新增文本样式数据表（未实装）

--- res/class/EditorConstructor.js
+++ res/class/EditorConstructor.js
@@ -1,6 +1,13 @@
+// 字段列表构造器
 class EditorTextList {
     constructor() {}
 
+    /**
+     * 项目
+     * @param {Number} index 索引编号
+     * @param {String} value 值
+     * @returns {String} DOM
+     */
     static getItem(index, value) {
         return `
         <div class="editor-text-list-item" data-index="${index}">
@@ -13,11 +20,93 @@ class EditorTextList {
         </div>`;
     }
 
+    /**
+     * 列表
+     * @param {String} value 值
+     * @returns {String} DOM
+     */
     static getList(value) {
         let dom = '';
         for (let i = 0; i < value.length; i++) {
             dom += this.getItem(i, value[i]);
         }
         return dom;
+    }
+}
+
+// 表单构造器
+class EditorForm {
+    constructor() {}
+
+    /**
+     * 提示
+     * @param {String} label 标签名称
+     * @returns {String} DOM
+     */
+    static tip(label) {
+        return `<div class="abuser-from-checkbox-list"><span class="abuser-from-input-tip">${label}</span></div>`
+    }
+
+    /**
+     * 表单项目
+     * @param {String} id ID
+     * @param {String} label 标签名称
+     * @param {String} content 内容
+     * @param {String} tip 提示
+     * @returns {String} DOM
+     */
+    static item(id, label, content, tip = '') {
+        return `<div class="abuser-from-row"><label for="${id}">${label}</label>${content}</div>${tip != '' ? FormConstructor.tip(tip) : ''}`;
+    }
+
+    /**
+     * 输入框
+     * @param {String} id ID
+     * @param {String} label 标签名称
+     * @param {String} def 默认值
+     * @param {String} tip 提示
+     * @param {String} type 类型
+     * @returns {String} DOM
+     */
+    static input(id, label, def, tip = '', type = 'text') {
+        return FormConstructor.item(
+            id,
+            label,
+            `<input type="${type}" name="${id}" id="${id}" class=" abuser-form-item" value="${def}" data-default="${def}">`,
+            tip
+        );
+    }
+
+    /**
+     * 数字输入框
+     * @param {String} id ID
+     * @param {String} label 标签名称
+     * @param {String} def 默认值
+     * @param {String} tip 提示
+     * @returns {String} DOM
+     */
+    static inputNum(id, label, def, tip = '') {
+        return FormConstructor.input(id, label, def, tip, 'number');
+    }
+
+    /**
+     * 复选框
+     * @param {String} id ID
+     * @param {String} label 标签名称
+     * @param {String} def 默认值
+     * @param {String} tip 提示
+     * @returns {String} DOM
+     */
+    static checkbox(id, label, def, tip = '') {
+        return FormConstructor.item(
+            id,
+            '',
+            `<button role="checkbox" aria-selected="${def}" class="checkbox ${def == 1 ? 'selected' : ''}">
+                <span class="icon"></span>
+                <span class="text">${label}</span>
+                <input type="hidden" name="${id}" id="${id}" value="${def}" data-default="${def}">
+            </button>`,
+            tip
+        );
     }
 }

2faa40a39f321f61bed485bfc3d9c3defc661cbf
sheep-realms <sheep_realms@qq.com>
2024-04-28 15:29:05 +0800

    前台页面新增 theme URL 参数

--- res/script/history.js
+++ res/script/history.js
@@ -8,7 +8,8 @@ if (config.history.latest_message_hide) $('body').addClass('echo-live-history-la
 
 let echoLiveHistory = new EchoLiveHistory(config);
 echoLiveHistory.theme = extensionManager.theme;
-echoLiveHistory.setTheme(config.history.history_theme || config.global.theme);
+let urlTheme = EchoLiveTools.getUrlParam('theme');
+echoLiveHistory.setTheme(urlTheme || config.history.history_theme || config.global.theme);
 
 echoLiveHistory.on('newHistory', function(e) {
     sendHistory(e.username, e.message);
--- res/script/live.js
+++ res/script/live.js
@@ -4,7 +4,8 @@ let echo = new Echo();
 if (config.echo.html_format_enable != true) echo.filter.HTMLFormat = false;
 let echolive = new EchoLive(echo, config);
 echolive.theme = extensionManager.theme;
-echolive.setTheme(config.echolive.style.live_theme || config.global.theme);
+let urlTheme = EchoLiveTools.getUrlParam('theme');
+echolive.setTheme(urlTheme || config.echolive.style.live_theme || config.global.theme);
 
 let data;
 
